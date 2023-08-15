eeacb3670f85710c563ee5d2af4f7d3b4b3b5946
dkang1630 <107570892+dkang1630@users.noreply.github.com>
2023-08-15 15:06:59 -0500

    Add files via upload

---
+++ Ultrasonic_polar.m
@@ -0,0 +1,28 @@
+theta = 0:(pi/180):pi;
+s = serial('COM4');
+s.BaudRate=9600
+fopen(s)
+i = 0;
+
+inc = 1;
+
+while i<180
+   A = fgets(s);
+   num(i+1) = str2num(A);
+   i = i+1;
+end
+fclose(s)
+
+j = 1
+
+while j<181
+    tab(j,1) = (j-1)*inc
+    tab(j,2) = num(j)
+    tab(j,3) = num(j)*cosd((j-1)*inc)
+    tab(j,4) = num(j)*sind((j-1)*inc)
+    j = j+1
+end
+%figure
+%polar(theta,num)
+
+plot(tab(:,3),tab(:,4))
